8dd2c592c3f328a13219187fa4723deb6dcf58cc
Scott W <scott@halofx.com>
2015-12-03 11:49:00 -0600

    Update shutdown.py

--- shutdown.py
+++ shutdown.py
@@ -9,14 +9,14 @@ import os
 # Use the Broadcom SOC Pin numbers  
 # Setup the Pin with Internal pullups enabled and PIN in reading mode.  
 GPIO.setmode(GPIO.BCM)  
-GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
+GPIO.setup(3, GPIO.IN, pull_up_down = GPIO.PUD_UP)  
  
 # Our function on what to do when the button is pressed  
 def Shutdown(channel):  
     os.system("sudo shutdown -h now")  
  
 # Add our function to execute when the button pressed event happens  
-GPIO.add_event_detect(18, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)  
+GPIO.add_event_detect(3, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)  
  
 # Now wait!  
 while 1:  
